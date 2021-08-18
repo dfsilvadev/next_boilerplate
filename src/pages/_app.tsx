ea0637160823d56eb65f75c2ec5dd1ad364a443c
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-18 16:33:32 -0300

    feat: configurando os primeiros resets do styles global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,32 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import GlobalStyles from "styles/global";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link
+          rel="shortcut icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Main from "components/Main";
+
 export default function Home() {
-  return <h1>app</h1>;
+  return <Main />;
 }
