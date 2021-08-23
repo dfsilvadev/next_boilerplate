ae2c94f9dbc9f40ba32e4910b623a55369e5f0b3
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-23 14:31:36 -0300

    chore: configurando o Essentials e Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,12 +1,10 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { withKnobs, text } from "@storybook/addon-knobs";
 
 import Main from ".";
 
 export default {
   title: "Main",
   component: Main,
-  decorators: [withKnobs],
   args: {
     title: "React Avançado",
     description: "TypeScript, React, NextJS e Styled Componets"
@@ -14,10 +12,5 @@ export default {
 } as Meta;
 
 export const Basic: Story = ({ title, description }) => {
-  return (
-    <Main
-      title={text("Title", title)}
-      description={text("Description", description)}
-    />
-  );
+  return <Main title={title} description={description} />;
 };
