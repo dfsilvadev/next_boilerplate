24f37a52513c58ecd743d8a84d23fc5ca3ae75f8
Daniel Silva <dani3l.dxp@gmail.com>
2021-11-01 12:58:08 -0300

    testing

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,20 +5,23 @@ type MainProps = {
   description?: string;
 };
 
-const Main = ({ title, description }: MainProps) => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado"
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
+const Main = ({ title, description }: MainProps) => {
+  const foo = "bar";
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
 
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código"
-    />
-  </S.Wrapper>
-);
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código"
+      />
+    </S.Wrapper>
+  );
+};
 
 export default Main;
